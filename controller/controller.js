1998883d1d66d09fd90d5d541193f49d33f06e83
soutt0 <soutt0.script@gmail.com>
2026-08-24 17:35:06 -0300

    IP da EC2 att + SSH funcionando

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// http://localhost:3000/
+// http://localhost:3000/ 
 
 router.get('/', (req, res) => {
     res.status(200).json({ success: true, data: 'healthy!' });
